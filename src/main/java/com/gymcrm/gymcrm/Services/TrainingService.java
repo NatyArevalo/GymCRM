74d1ad65b68f65aab105d9ae2d8bf6f80a7a1b29
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-02 20:45:39 -0500

    Add docker and ActiveMQ

--- src/main/java/com/gymcrm/gymcrm/Services/TrainingService.java
+++ src/main/java/com/gymcrm/gymcrm/Services/TrainingService.java
@@ -42,6 +42,8 @@ public class TrainingService {
     @Autowired
     TrainerBillingClient trainerBillingClient;
 
+    @Autowired
+    TrainingSender trainingSender;
 
 
     public Training createTraining(TrainingDTO trainingDTO){
@@ -92,7 +94,7 @@ public class TrainingService {
         }
         trainingRepository.save(training);
         TrainingBillingDTO billingDTO = trainingBillingMapper.mapToDTO(training);
-        notifyBillingService(billingDTO);
+        trainingSender.sendTrainingData(billingDTO);
         return training;
     }
     @Retry(name = "trainerBillingService", fallbackMethod = "fallbackNotify")
