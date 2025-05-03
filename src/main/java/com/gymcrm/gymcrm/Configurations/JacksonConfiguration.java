0dd26121c78ee71eb2fed5d031b6c2e7b200eec8
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 18:51:07 -0500

    Jackson Configuration

--- src/main/java/com/gymcrm/gymcrm/Configurations/JacksonConfiguration.java
+++ src/main/java/com/gymcrm/gymcrm/Configurations/JacksonConfiguration.java
@@ -1,16 +1,16 @@
 package com.gymcrm.gymcrm.Configurations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfiguration {
     @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.modules(new JavaTimeModule());
-        return builder;
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());  // Register JavaTimeModule
+        return objectMapper;
     }
 }
