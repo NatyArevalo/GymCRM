a79a266cca4eafc683b695b57902815909691d5f
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 20:19:46 -0500

    jacksonJmsMessageConverter

--- src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
+++ src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
@@ -1,5 +1,6 @@
 package com.gymcrm.gymcrm.Configurations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -9,10 +10,11 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfiguration {
     @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
