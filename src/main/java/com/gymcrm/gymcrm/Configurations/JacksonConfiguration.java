0846c1ece514c2e07b90fd74093d2194aab6ef24
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-03 20:32:54 -0500

    jacksonJmsMessageConverter

---
+++ src/main/java/com/gymcrm/gymcrm/Configurations/JacksonConfiguration.java
@@ -0,0 +1,16 @@
+package com.gymcrm.gymcrm.Configurations;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+
+public class JacksonConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
