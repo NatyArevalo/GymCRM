91f400f71a0a037176b8bd2391ec82e412763055
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-04 22:04:36 -0500

    message converter

--- src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
+++ src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfiguration {
     @Bean
-    public MessageConverter JmsMessageConverter(ObjectMapper objectMapper) {
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
