53aba6382e1710735839e5a44bacadc9bceaaa5c
nataliaArevalo <natiarevalo32@hotmail.com>
2025-05-05 20:56:06 -0500

    Adding setTypeIdMappings from JmsConfiguration

--- src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
+++ src/main/java/com/gymcrm/gymcrm/Configurations/JmsConfiguration.java
@@ -1,11 +1,14 @@
 package com.gymcrm.gymcrm.Configurations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gymcrm.gymcrm.DTO.TrainingBillingDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+
+import java.util.Map;
 
 @Configuration
 public class JmsConfiguration {
@@ -14,6 +17,9 @@ public class JmsConfiguration {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_typeId");
+        converter.setTypeIdMappings(Map.of(
+                "TrainingBillingDTO", TrainingBillingDTO.class
+        ));
         converter.setObjectMapper(objectMapper);
         return converter;
     }
